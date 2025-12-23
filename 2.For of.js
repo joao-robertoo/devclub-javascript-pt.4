b2cb6d8b656b374afec4627009eb5ccf88d04d88
J <joao.adsbrasil@gmail.com>
2025-12-23 19:30:28 -0300

    For of

---
+++ 2.For of.js
@@ -0,0 +1,12 @@
+/* 
+Estrutura de Repetição - LOOP
+
+    -FOR OF
+*/
+
+const myName = 'João'
+const users = ['Rodolfo', 'Maria', 'José']
+
+for (const letter of myName) {
+    console.log(letter)
+}
