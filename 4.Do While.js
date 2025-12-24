ee7ff02df37116e0c1cc2c4938489b12abd8f9b4
J <joao.adsbrasil@gmail.com>
2025-12-23 23:06:43 -0300

    Do while

---
+++ 4.Do While.js
@@ -0,0 +1,17 @@
+/* 
+    While & do while
+
+    while -> Verifique se e TRUE e Faça
+    do while -> Faça, depois veja se e TRUE
+*/
+let i = 0
+
+while (i < 30) {
+    i++;
+    console.log(i)
+}
+
+do {
+    i++;
+    console.log(i)
+} while (i < 20);
